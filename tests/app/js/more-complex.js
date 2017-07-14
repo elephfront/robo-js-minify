2eb9373cd674d4095e2f144df6894c457b8d4b47
Yves P <havokinspiration@gmail.com>
2017-07-14 17:02:23 +0200

    Fix the tests

--- tests/app/js/more-complex.js
+++ tests/app/js/more-complex.js
@@ -2,3 +2,9 @@ var msg = 'Hello';
 alert(msg);
 
 var thisIsABool = true;
+
+function isItBool(variable) {
+    return variable === true || variable === false;
+}
+
+alert(isItBool(thisIsABools));
---
+++ tests/comparisons/testComplexMinification.js
@@ -0,0 +1,2 @@
+var msg='Hello';alert(msg);var thisIsABool=!0;function isItBool(variable){return variable===!0||variable===!1}
+alert(isItBool(thisIsABools))
